2d83b5a016357fec05260955129c9c87aed5d764
shuvo-sarkaR-sys <md.sirforce@gmail.com>
2024-09-18 12:26:23 +0600

    code is update

--- live-restourent/src/App.jsx
+++ live-restourent/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
- 
+
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Home from './pages/Home/Home'
@@ -12,19 +12,20 @@ import Login from './components/LoginPopUp/Login'
 
 
 const App = () => {
-  
-const [showLogin,setShowLogin] = useState(false)
+
+  const [showLogin, setShowLogin] = useState(false)
 
   return (
-   <>{showLogin ? <Login setShowLogin={setShowLogin}/>:<></>} <div className='app'>
-      <Navbar setShowLogin={setShowLogin}/>
-      <Routes>
-        <Route path='/' element={< Home/>} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/order' element={<PlaceOrder />} />
-      </Routes>
-    </div>
-    <Footer />
+    <>{showLogin ? <Login setShowLogin={setShowLogin} /> : <></>}
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path='/' element={< Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
   )
 }
--- live-restourent/src/components/FoodDisplay/FoodDisplay.jsx
+++ live-restourent/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,77 +1,72 @@
-// import React, { useContext } from 'react'
-// import './FoodDisplay.css'
-//  import { StoreContext } from '../../StoreContext/StoreContext';
-// import FoodItem from '../FoodItem/FoodItem';
-
-
-// const FoodDisplay = ({category}) => {
-//   const {food_list} = useContext( StoreContext);
-
-  
-
-//     return (
-
-//     <div className='food-display' id='food-display'>
-//       <h2>Top dishes near your</h2>
-//       <div className="food-display-list">
-//         {food_list.map((item,index)=>{
-//           console.log(category,item.category);
-//             if(category ==="All" || category===item.category )
-//             {
-//                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
-//             }
-            
-
-//             return null;
-//         })}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default FoodDisplay
-import React, { useContext } from 'react';
-import './FoodDisplay.css';
-import { StoreContext } from '../../StoreContext/StoreContext';
+import React, { useContext, useState } from 'react'
+import './FoodDisplay.css'
+ import { StoreContext } from '../../StoreContext/StoreContext';
 import FoodItem from '../FoodItem/FoodItem';
 
-const FoodDisplay = ({ category }) => {
-  const { food_list } = useContext(StoreContext);
 
-  return (
+const FoodDisplay = ({category}) => {
+  const {food_list} = useContext( StoreContext);
+
+ 
+    return (
+
     <div className='food-display' id='food-display'>
-      <h2>Top dishes near you</h2>
+      <h2>Top dishes near your</h2>
+       
       <div className="food-display-list">
-        <h3>All Food Items</h3>
-        {food_list.map((item, index) => (
-          <FoodItem
-            key={index}
-            id={item._id}
-            name={item.name}
-            description={item.description}
-            price={item.price}
-            image={item.image}
-          />
-        ))}
-        <h3>Filtered Food Items for Category: {category}</h3>
-        {food_list.map((item, index) => {
-          if (category === "All" || category === item.category) {
-            return (
-              <FoodItem
-                key={index}
-                id={item._id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-                image={item.image}
-              />
-            );
-          }
-          return null;
+        {food_list.map((item,index)=>{
+          console.log(category,item.category);
+            if(category ==="all" || category===item.category )
+            {
+               return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+            }
+            
+
+            return null;
         })}
       </div>
     </div>
-  );
+  )
 }
 
-export default FoodDisplay;
+export default FoodDisplay
+
+
+// import React, { useContext } from 'react';
+// import './FoodDisplay.css';
+// import { StoreContext } from '../../StoreContext/StoreContext';
+// import FoodItem from '../FoodItem/FoodItem';
+
+// const FoodDisplay = ({ category }) => {
+//   const { food_list } = useContext(StoreContext);
+
+//   return (
+//     <div className='food-display' id='food-display'>
+//       <h2>Top dishes near you</h2>
+//       <div className="food-display-list">
+//         <h3>All Food Items</h3>
+         
+//         <h3>Filtered Food Items for Category: {category}</h3>
+//         {food_list.map((item, index) => {
+//           if (category === "all" || category === item.category) {
+//             return (
+//               <FoodItem
+//                 key={index}
+//                 id={item._id}
+//                 name={item.name}
+//                 description={item.description}
+//                 price={item.price}
+//                 image={item.image}
+//               />
+//             );
+            
+//           }
+         
+//           return null;
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default FoodDisplay;
